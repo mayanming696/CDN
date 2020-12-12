2c2e48b745063054ad2640d5e6fbf547572e525e
mayanming696 <543229218@qq.com>
2020-12-12 16:13:18 +0800

    提交JS

---
+++ js/Blog.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+	var typed = new Typed(".subtitle", {
+		strings: ['Hope is a good thing,', 'maybe the best of things,', 'and no good thing ever dies.'],
+		startDelay: 100,
+		typeSpeed: 100,
+		loop: !0,
+		backSpeed: 50,
+		showCursor: !0
+	});
+}
